2eedafe888a1360c61f1e355f456c1d20c8ffe23
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-09-27 11:48:17 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,28 @@
+﻿using TareaCasaC.Servicios;
+
+namespace TareaCasaC
+{
+    /// <summary>
+    /// clase principal de la entrada
+    /// </summary>
+    class Program
+
+        /// <summary>
+        /// metodo de entrada de la aplicacion
+        /// 270923-amd
+        /// </summary>
+        /// <param name="args"></param>
+
+       static void Main(string[] args);
+    {
+    
+
+
+
+}
+
+
+}
+
+
+
---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaCasaC.servicios
+  {   /// <summary>
+      /// interfaz que corresponde a las acciones que puede hacer el menu
+      /// </summary>
+    internal interface MenuInterfaz
+    {
+        /// <summary>
+        /// muestra el mensaje bienvenida
+        /// 270923
+        /// </summary>
+        public void mensajeBienvenida();
+
+        /// <summary>
+        /// metodo que muestra el menu y recoge la eleccion del usuario
+        /// </summary>
+        /// <returns>un enetero con la opciion elegida</returns>
+        public int mostrarMunuYSeleccionar();
+    }
+}
