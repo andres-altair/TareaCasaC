e97e6b1169ef6123384d6c5141f98922440b864e
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-10-02 14:05:50 +0200

    no message

--- Controladores/Program.cs
+++ Controladores/Program.cs
@@ -1,4 +1,5 @@
-﻿using TareaCasaC.Servicios;
+﻿using TareaCasaC.servicios;
+
 
 namespace TareaCasaC
 {
@@ -7,22 +8,70 @@ namespace TareaCasaC
     /// </summary>
     class Program
 
-        /// <summary>
-        /// metodo de entrada de la aplicacion
-        /// 270923-amd
-        /// </summary>
-        /// <param name="args"></param>
-
-       static void Main(string[] args);
+      /// <summary>
+      /// metodo de etrada de la aplicacion
+      /// 270923-amd
+      /// </summary>
+      /// <param name="args"></param>
+        static void Main(String[] args)
     {
-    
+        {
+
+
+            MenuInterfaz menuInterfaz = new MenuImplenetacion();
+            menuInterfaz.mensajeBienvenida();
+            //Variable que controla la entrada y salida del bucle while
+           
+            bool cerrarMenu = false;
+            //Va a contener la opcion seleccionada
+            int opcionSeleccionar;
+
+            //Si da true dentro, entrara, si no, no
+            //Desde la primera iteracion debe cumplirse la condicion
+            while (!cerrarMenu)
+            //tambien esta el do while
+
+            {
+                opcionSeleccionar = menuInterfaz.mostrarMunuYSeleccionar();
+                Console.WriteLine(opcionSeleccionar);
+                //coge la opcion del usuario y la copia en la opcion seleccionada
+
+                switch (opcionSeleccionar)
+                //el while y switch van juntos
+                {
+                    case 0:
+                        Console.WriteLine("[INFO] - se ejecuta caso 0.");
+                        cerrarMenu = true;
+
+                        break;//Sirve para que se salga del switch y vaya a la siguiente linea
+                    case 1:
+                        Console.WriteLine("[INFO] - se ejecuta caso 1.");
+                        break;
+                    case 2:
+                        Console.WriteLine("[INFO] - se ejecuta caso 2.");
+                        break;
+                    
+                    default:
+                        Console.WriteLine("[INFO] - La opcion seleccionada no esta disponible.");
+                        //este sirve para dar una informacion
+                        break;
+                }
 
 
 
+            }
+        }
+    }
 }
 
 
-}
+
+
+
+        
+
+
+
 
 
 
--- Servicios/MenuImplenetacion.cs
+++ Servicios/MenuImplenetacion.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TareaCasaC.servicios;
 
 namespace TareaCasaC.Servicios
 {
     internal class MenuImplenetacion : MenuInterfaz
 
     {
+        /// <summary>
+        /// clase que implementa la interfaz de menu
+        /// 250923-amd
+        /// </summary>
+
+        public void mostrarMensajeBienevenida()
+        {
+            string mensaje = "Bienvenido Andres"
+                Console.WriteLine(mensaje);
+        }
+        public int mostrarMenuYSeleccion()
+        {
+            int opcionIntroducida;
+
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("0. Cerrar aplicacion ");
+            Console.WriteLine("1. Examenes");
+            Console.WriteLine("2. Ejercicios de hoy");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("Selecione una opcion");
+            opcionIntroducida = Console.ReadKey(true).KeyChar - ('0');
+            return opcionIntroducida;   
+        }
+
+
     }
 }
